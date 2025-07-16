4aa6454533164d1d39663ae82cd574711ff4e7fb
Sachin Kushawaha <sk8228090@gmail.com>
2025-07-17 00:08:22 +0530

    Remove history configuration files and update sdkconfig.defaults
    - Deleted sdkconfig.ci.history and sdkconfig.ci.nohistory files to remove history tracking.
    - Increased main task stack size from 7168 to 8192.
    - Added configurations for ESPTool flash mode and flash size.
    - Set partition table offset to 0x9000.
    - Enabled compiler optimization for performance.
    - Removed unused configurations related to crystal frequency and console output settings.

--- main/esp32_nat_router.c
+++ main/esp32_nat_router.c
@@ -17,6 +17,7 @@
 #include "driver/uart.h"
 #include "esp_vfs_usb_serial_jtag.h"
 #include "driver/usb_serial_jtag.h"
+#include "driver/uart_vfs.h"
 #include "linenoise/linenoise.h"
 #include "argtable3/argtable3.h"
 #include "esp_vfs_fat.h"
@@ -247,9 +248,10 @@ static void initialize_console(void)
     fsync(fileno(stdout));
     
     /* Minicom, screen, idf_monitor send CR when ENTER key is pressed */
-    esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
+    uart_vfs_dev_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
+    // esp_vfs_dev_uart_port_set_rx_line_endings(0, ESP_LINE_ENDINGS_CR);
     /* Move the caret to the beginning of the next line on '\n' */
-    esp_vfs_dev_uart_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
+    uart_vfs_dev_port_set_tx_line_endings(0, ESP_LINE_ENDINGS_CRLF);
 
     /* Configure UART. Note that REF_TICK is used so that the baud rate remains
      * correct while APB frequency is changing in light sleep mode.
@@ -271,7 +273,7 @@ static void initialize_console(void)
     ESP_ERROR_CHECK( uart_param_config(CONFIG_ESP_CONSOLE_UART_NUM, &uart_config) );
 
     /* Tell VFS to use UART driver */
-    esp_vfs_dev_uart_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
+   uart_vfs_dev_use_driver(CONFIG_ESP_CONSOLE_UART_NUM);
 #endif
 
 #if CONFIG_ESP_CONSOLE_USB_SERIAL_JTAG
